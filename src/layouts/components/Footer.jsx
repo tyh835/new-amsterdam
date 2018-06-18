59c107cbbeacdbbcd9502b9ae9f89d85100d2d03
Tony Han <itony9401@live.com>
2018-06-18 02:19:53 -0700

    fixed import path for Disclaimer.jsx

--- src/layouts/components/Footer.jsx
+++ src/layouts/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Disclaimer from '../Disclaimer.jsx';
+import Disclaimer from './Disclaimer.jsx';
 
 const Footer = props => {
   return (
