288bf487a28c3c892676d1c0d39210d37063e45e
Tony Han <itony9401@live.com>
2018-06-18 02:05:56 -0700

    injected styled-components to cms

--- src/cms/preview-templates/IndexPagePreview.jsx
+++ src/cms/preview-templates/IndexPagePreview.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Provider} from 'rebass';
+import {StyleSheetManager} from 'styled-components';
 
 import { IndexPageTemplate } from '../../templates/index-page';
 import theme from '../../layouts/theme.js';
 
 const IndexPagePreview = ({ entry, getAsset }) => {
+  const iframe = document.querySelector(".nc-previewPane-frame")
+  const iframeHeadElem = iframe.contentDocument.head;
+
   const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs']);
   const blurbs = entryBlurbs ? entryBlurbs.toJS() : [];
 
   return (
-    <Provider theme={theme}>
-      <IndexPageTemplate
-        image={entry.getIn(['data', 'image'])}
-        title={entry.getIn(['data', 'title'])}
-        heading={entry.getIn(['data', 'heading'])}
-        description={entry.getIn(['data', 'description'])}
-        intro={{ blurbs }}
-      />
-    </Provider>
+    <StyleSheetManager target={iframeHeadElem}>
+      <Provider theme={theme}>
+        <IndexPageTemplate
+          image={entry.getIn(['data', 'image'])}
+          title={entry.getIn(['data', 'title'])}
+          heading={entry.getIn(['data', 'heading'])}
+          description={entry.getIn(['data', 'description'])}
+          intro={{ blurbs }}
+        />
+      </Provider>
+    </StyleSheetManager>
   );
 };
 
