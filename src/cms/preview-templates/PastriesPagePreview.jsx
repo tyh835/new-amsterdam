523b2386d924f04966ec98028bab42680d8457b1
Tony Han <itony9401@live.com>
2018-06-28 21:08:38 -0700

    fixed pastries preview import

--- src/cms/preview-templates/PastriesPagePreview.jsx
+++ src/cms/preview-templates/PastriesPagePreview.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { Provider } from 'rebass';
 import { StyleSheetManager } from 'styled-components';
 
-import { PastriesPageTemplate } from '../../templates/index-page.jsx';
+import { PastriesPageTemplate } from '../../templates/pastries-page.jsx';
 import theme from '../../layouts/theme.js';
 
 const PastriesPagePreview = ({ entry }) => {
   const iframe = document.querySelector('.nc-previewPane-frame');
   const iframeHeadElement = iframe.contentDocument.head;
 
-  const cardsPre = entry.getIn(['data', 'pastries']);
-  const cards = cardsPre ? cardsPre.toJS() : [];
+  const pastriesPre = entry.getIn(['data', 'pastries']);
+  const pastries = pastriesPre ? pastriesPre.toJS() : [];
 
   return (
     <StyleSheetManager target={iframeHeadElement}>
@@ -19,7 +19,7 @@ const PastriesPagePreview = ({ entry }) => {
         <PastriesPageTemplate
           title={entry.getIn(['data', 'title'])}
           jumbotron={entry.getIn(['data', 'jumbotron'])}
-          pastries={cards}
+          pastries={pastries}
           isPreview={true}
         />
       </Provider>
