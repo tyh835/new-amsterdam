b9475551de7439f7c8864ff9fb3c4c017e647733
Tony Han <itony9401@live.com>
2018-06-30 15:36:13 -0700

    revert to simple stateless form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,86 +1,23 @@
 import React, {Component} from "react";
-import { navigateTo } from "gatsby-link";
-import Recaptcha from "react-google-recaptcha";
 
-import {encode} from '../utils/utils.js';
-
-const RECAPTCHA_KEY = "6Le6lGEUAAAAAPitKLCGmirC8TIDmylpuux0u9F3";
-export default class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {name: '', email: '', message: '', 'g-recaptcha-response': ''};
-  }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleRecaptcha = value => {
-    this.setState({ "g-recaptcha-response": value });
-  };
-
-  handleSubmit = e => {
-    console.log('submit');
-    console.log(encode({
-      "form-name": 'contact',
-      ...this.state
-    }));
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": 'contact',
-        ...this.state
-      })
-    })
-      .then(alert('success'))
-      .catch(error => alert(error));
-  };
-
+export class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact Us</h1>
-        <form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-recaptcha="true"
-          onSubmit={this.handleSubmit}
-        >
-          <noscript>
-            <p>This form won’t work with Javascript disabled</p>
-          </noscript>
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <Recaptcha
-            ref="recaptcha"
-            sitekey={RECAPTCHA_KEY}
-            onChange={this.handleRecaptcha}
-          />
-          <p>
-            <button className="g-recaptcha" type="submit">Send</button>
-          </p>
-        </form>
-      </div>
-    );
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" /> 
+        <p>
+          <label>Name: <input type="text" name="name" /></label>
+        </p> 
+        <p>
+          <label>Email: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <div data-netlify-recaptcha></div>
+        <p>
+          <button type='submit'>Send</button>
+        </p>
+      </form>);
   }
 }
