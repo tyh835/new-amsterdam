32c999e3cce6025bde7d43b2ab5d41f6afb19b30
Tony Han <itony9401@live.com>
2018-06-30 16:29:24 -0700

    added state to form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,18 +1,23 @@
 import React, {Component} from "react";
 
 export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
   render() {
     return (
       <form name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" /> 
         <p>
-          <label>Name: <input type="text" name="name" /></label>
+          <label>Name: <input type="text" name="name" value={name} onChange={this.handleChange} /></label>
         </p> 
         <p>
-          <label>Email: <input type="text" name="name" /></label>
+          <label>Email: <input type="text" name="name" value={email} onChange={this.handleChange} /></label>
         </p>
         <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label>Message: <textarea name="message" value={message} onChange={this.handleChange}></textarea></label>
         </p>
         <div data-netlify-recaptcha></div>
         <p>
