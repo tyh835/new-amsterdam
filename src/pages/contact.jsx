6d069a7a5dc54345a9792651a6ca9442c3d9033c
Tony Han <itony9401@live.com>
2018-06-30 22:13:24 -0700

    added success page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,10 +4,16 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = { name: "", email: "", message: "" };
+    this.validate = false;
   }
 
   handleSubmit = e => {
-    
+    // make a field validation step
+    this.validate = true;
+    if (!this.validate) {
+      e.preventDefault();
+    }
+    // allow default behaviour if fields have been validated
   }
 
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -15,7 +21,7 @@ export default class Contact extends Component {
   render() {
     const {name, email, message} = this.state;
     return (
-      <form name="contact" method="POST" data-netlify="true" onSubmit={this.handleSubmit}>
+      <form name="contact" method="POST" data-netlify="true" action="/success" onSubmit={this.handleSubmit}>
         <input type="hidden" name="form-name" value="contact" /> 
         <label>Name: </label><input type="text" name="name" value={name} onChange={this.handleChange} />
         <label>Email: </label><input type="text" name="email" value={email} onChange={this.handleChange} />
---
+++ src/pages/success.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      Thank you for contacting us!
+    </div>
+  )
+}
