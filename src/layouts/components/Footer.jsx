3fbe6eedba6536fab6572375659e29c59f508bbe
Tony Han <itony9401@live.com>
2018-10-09 10:55:08 -0700

    added shadow to footer box

--- src/layouts/components/Footer.jsx
+++ src/layouts/components/Footer.jsx
@@ -22,7 +22,7 @@ const InfoBox = Flex.extend`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  box-shadow: 0 5px 8px rgba(0, 0, 0, 0.16), 0 5px 8px rgba(0, 0, 0, 0.23);
 
   @media (max-width: ${props => props.theme.breakpoints[1]}) {
     height: 180px;
