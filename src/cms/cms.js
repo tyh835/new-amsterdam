86cd10683be1e7607aac627f6d1f3bd84fb867b3
Tony Han <itony9401@live.com>
2018-10-10 22:11:07 -0700

    removed previews from cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,7 @@
 import CMS from 'netlify-cms';
 
-import IndexPagePreview from './preview-templates/IndexPagePreview.jsx';
-import CakesPagePreview from './preview-templates/CakesPagePreview.jsx';
-import PastriesPagePreview from './preview-templates/PastriesPagePreview.jsx';
+import BlankPreview from './preview-templates/BlankPreview.jsx';
 
-CMS.registerPreviewStyle('/styles.css');
-CMS.registerPreviewTemplate('index', IndexPagePreview);
-CMS.registerPreviewTemplate('cakes', CakesPagePreview);
-CMS.registerPreviewTemplate('pastries', PastriesPagePreview);
+CMS.registerPreviewTemplate('index', BlankPreview);
+CMS.registerPreviewTemplate('cakes', BlankPreview);
+CMS.registerPreviewTemplate('pastries', BlankPreview);
---
+++ src/cms/preview-templates/BlankPreview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const BlankPreview = () => {
+  return (
+    <div>
+      This is no preview.
+    </div>
+  );
+};
+
+export default BlankPreview;
