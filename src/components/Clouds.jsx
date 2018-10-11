a96fed6e552bcf1d558bb6d8a675f0eea59c9081
Tony Han <itony9401@live.com>
2018-10-11 11:14:48 -0700

    refactored clouds out of message-page

---
+++ src/components/Clouds.jsx
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+import { moveclouds } from '../utils/animations.js';
+
+// Floating Clouds LOL
+const Cloud = styled.div`
+  width: 350px;
+  height: 120px;
+  background-color: #fff;
+  background: linear-gradient(top, #fff 100%);
+  border-radius: 100px;
+  position: absolute;
+  margin: 120px auto 20px;
+  z-index: 3;
+  transition: all 1s ease;
+
+  &:after,
+  &:before {
+    content: '';
+    position: absolute;
+    background: #fff;
+    z-index: 3;
+  }
+
+  &:after {
+    width: 100px;
+    height: 100px;
+    top: -50px;
+    left: 50px;
+    border-radius: 100px;
+  }
+
+  &:before {
+    width: 180px;
+    height: 180px;
+    top: -90px;
+    right: 50px;
+    border-radius: 200px;
+  }
+`;
+
+export const Cloud1a = styled(Cloud)`
+  top: 30px;
+  left: 100px;
+  transform: scale(0.3);
+  opacity: 0.9;
+  animation: ${moveclouds} 15s linear infinite;
+`;
+
+export const Cloud1b = styled(Cloud)`
+  top: 0px;
+  left: 250px;
+  transform: scale(0.3);
+  animation: ${moveclouds} 15s linear infinite;
+`;
+
+export const Cloud2 = styled(Cloud)`
+  left: 250px;
+  top: 100px;
+  transform: scale(0.6);
+  opacity: 0.6;
+  animation: ${moveclouds} 25s linear infinite;
+`;
+
+export const Cloud3 = styled(Cloud)`
+  left: 250px;
+  bottom: -70px;
+  transform: scale(0.6);
+  opacity: 0.8;
+  animation: ${moveclouds} 25s linear infinite;
+`;
+
+export const Cloud4 = styled(Cloud)`
+  left: 470px;
+  bottom: 50px;
+  transform: scale(0.75);
+  opacity: 0.75;
+  animation: ${moveclouds} 18s linear infinite;
+`;
+
+export const Cloud5 = styled(Cloud)`
+  left: 200px;
+  top: 300px;
+  transform: scale(0.5);
+  opacity: 0.8;
+  animation: ${moveclouds} 20s linear infinite;
+`;
--- src/templates/message-page.jsx
+++ src/templates/message-page.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { Flex } from 'rebass';
 
 import theme from '../styles/theme.js';
-import { moveclouds } from '../utils/animations.js';
+import { Cloud1a, Cloud1b, Cloud2, Cloud3, Cloud4, Cloud5 } from '../components/Clouds.jsx';
 
 const Wrap = styled(Flex)`
   float: left;
@@ -25,90 +25,6 @@ const Title = styled.h1`
 const Text = styled.p`
   text-align: center;
   z-index: 5;
-`;
-
-// Floating Clouds LOL
-const Cloud = styled.div`
-  width: 350px;
-  height: 120px;
-  background-color: #fff;
-  background: linear-gradient(top, #fff 100%);
-  border-radius: 100px;
-  position: absolute;
-  margin: 120px auto 20px;
-  z-index: 3;
-  transition: all 1s ease;
-
-  &:after,
-  &:before {
-    content: '';
-    position: absolute;
-    background: #fff;
-    z-index: 3;
-  }
-
-  &:after {
-    width: 100px;
-    height: 100px;
-    top: -50px;
-    left: 50px;
-    border-radius: 100px;
-  }
-
-  &:before {
-    width: 180px;
-    height: 180px;
-    top: -90px;
-    right: 50px;
-    border-radius: 200px;
-  }
-`;
-
-const Cloud1a = styled(Cloud)`
-  top: 30px;
-  left: 100px;
-  transform: scale(0.3);
-  opacity: 0.9;
-  animation: ${moveclouds} 15s linear infinite;
-`;
-
-const Cloud1b = styled(Cloud)`
-  top: 0px;
-  left: 250px;
-  transform: scale(0.3);
-  animation: ${moveclouds} 15s linear infinite;
-`;
-
-const Cloud2 = styled(Cloud)`
-  left: 250px;
-  top: 100px;
-  transform: scale(0.6);
-  opacity: 0.6;
-  animation: ${moveclouds} 25s linear infinite;
-`;
-
-const Cloud3 = styled(Cloud)`
-  left: 250px;
-  bottom: -70px;
-  transform: scale(0.6);
-  opacity: 0.8;
-  animation: ${moveclouds} 25s linear infinite;
-`;
-
-const Cloud4 = styled(Cloud)`
-  left: 470px;
-  bottom: 50px;
-  transform: scale(0.75);
-  opacity: 0.75;
-  animation: ${moveclouds} 18s linear infinite;
-`;
-
-const Cloud5 = styled(Cloud)`
-  left: 200px;
-  top: 300px;
-  transform: scale(0.5);
-  opacity: 0.8;
-  animation: ${moveclouds} 20s linear infinite;
 `;
 
 const MessagePageTemplate = ({ data }) => {
