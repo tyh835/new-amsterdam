eb85a19ec75fb5e2371f1043ec6134ae13996841
Tony Han <itony9401@live.com>
2018-12-15 13:41:29 -0800

    increased slide speed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -12,7 +12,7 @@ const CarouselBlock = styled.div`
 
 const Carousel = ({ images }) => {
   const settings = {
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
     autoplay: true,
     dots: false,
     pauseOnHover: false,
